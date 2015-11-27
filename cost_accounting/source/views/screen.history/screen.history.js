dddc9b6afec8771860d3172f10316028da06bd44
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-11-27 11:43:50 +0200

    add "history" screen

---
+++ cost_accounting/source/views/screen.history/screen.history.js
@@ -0,0 +1,37 @@
+RAD.view("screen.history", RAD.Blanks.ScrollableView.extend({
+
+    url: 'source/views/screen.history/screen.history.html',
+/*
+    onInitialize: function () {
+        
+    },
+    onNewExtras: function (extras) {
+        
+    },
+    onReceiveMsg: function (channel, data) {
+        
+    },
+    onStartRender: function () {
+        
+    },
+    onEndRender: function () {
+        
+    },
+    onBeforeAttach: function(){
+
+    },
+    onStartAttach: function () {
+        
+    },
+    onEndAttach: function () {
+        
+    },
+    onEndDetach: function () {
+        
+    },
+    onDestroy: function () {
+        
+    }
+*/
+
+}));
